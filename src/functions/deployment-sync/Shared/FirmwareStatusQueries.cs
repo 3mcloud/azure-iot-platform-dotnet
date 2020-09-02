38df1516c5d22faf68e6cecfdb085033bc2937df
AARM6ZZ <aarm6zz@mmm.com>
2020-09-02 18:44:22 +0530

    Changes not to depend on Case sensitivity

--- src/functions/deployment-sync/Shared/FirmwareStatusQueries.cs
+++ src/functions/deployment-sync/Shared/FirmwareStatusQueries.cs
@@ -11,8 +11,8 @@ namespace Mmm.Iot.Functions.DeploymentSync.Shared
         public static IDictionary<QueryType, string> Queries { get; set; } = new Dictionary<QueryType, string>()
         {
             { QueryType.APPLIED, @"SELECT deviceId from devices where configurations.[[{0}]].status = 'Applied'" },
-            { QueryType.SUCCESSFUL, @"SELECT deviceId FROM devices WHERE configurations.[[{0}]].status = 'Applied' AND properties.reported.firmware.fwUpdateStatus='Current'" },
-            { QueryType.FAILED, @"SELECT deviceId FROM devices WHERE configurations.[[{0}]].status = 'Applied' AND properties.reported.firmware.fwUpdateStatus='Error'" },
+            { QueryType.SUCCESSFUL, @"SELECT deviceId FROM devices WHERE configurations.[[{0}]].status = 'Applied' AND (properties.reported.firmware.fwUpdateStatus='Current' OR properties.reported.firmware.fwUpdateStatus='current')" },
+            { QueryType.FAILED, @"SELECT deviceId FROM devices WHERE configurations.[[{0}]].status = 'Applied' AND (properties.reported.firmware.fwUpdateStatus='Error' OR properties.reported.firmware.fwUpdateStatus='error')" },
         };
     }
 }
